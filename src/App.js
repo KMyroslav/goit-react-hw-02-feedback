ef81a7a53f72a639097dad940fea6cb38eadb3ff
KMyroslav <kukhtarukm@gmail.com>
2021-10-02 17:30:45 +0300

    Finishes feedback app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from "react";
+import Section from "./components/Section/Section";
+import Statistics from "./components/Statistics/Statistic";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Notification from "./components/Notification/Notification";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onFeedbackBtnClick = (value) => () => {
+    this.setState((prevState) => ({
+      [value]: prevState[value] + 1,
+    }));
+  };
+
+  countTotal = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  calculateFeedback = () => {
+    return this.state.good ? (this.state.good * 100) / this.countTotal() : 0;
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onFeedbackBtnClick={this.onFeedbackBtnClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotal() ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              countTotal={this.countTotal}
+              calculateFeedback={this.calculateFeedback}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
